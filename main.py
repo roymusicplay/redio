d3d77213ad607390162fe3c81f42dc0722badac1
ABHINAS ROY <85425565+roymusicplay@users.noreply.github.com>
2021-09-03 21:12:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ bot.run(main())
 bot.start()
 @bot.on_message(filters.command("restart") & filters.user(Config.ADMINS))
 def restart(client, message):
-    message.reply_text("**Restarting... Join @AsmSafone!**")
+    message.reply_text("**Restarting... Join @ABOUT_ABHINAS!**")
     Thread(
         target=stop_and_restart
         ).start()
